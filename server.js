841fbdbb64b146bd9521974d6455eb871a6a9130
Ivan Jakab <ivan.jakab@gauss.hr>
2020-06-14 16:04:00 +0200

    feat: added static node server

---
+++ server.js
@@ -0,0 +1,15 @@
+var fs = require('fs'),
+    http = require('http');
+
+http.createServer(function (req, res) {
+    const file = req.url !== '/' ? req.url : '/index.html'
+    fs.readFile(__dirname + file, function (err,data) {
+        if (err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+}).listen(8080);
