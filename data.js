e32d4030c24414f2d47e0f24159aa23c13d195a5
Ivan Jakab <ivan.jakab@gauss.hr>
2020-08-01 15:48:32 +0200

    chore: changed attributes

--- data.js
+++ data.js
@@ -1,22 +1,22 @@
 const numericFields = {
-    age: 'Broj godina',
+    freetime: 'Slobodno vrijeme',
     Medu: 'Edukacija majke',
     Fedu: 'Edukacija oca',
     traveltime: 'Vrijeme putovanja',
     studytime: 'Vrijeme učenja',
-    freetime: 'Slobodno vrijeme',
     goout: 'Vrijeme izlazaka',
-    absences: 'Izostanci',
-    G3: 'Uspjeh',
 }
 
 const keywords = {
+    absences: 'Izostanci',
+    age: 'Broj godina',
     sex: 'Spol',
     famsize: 'Veličina obitelji',
     Mjob: 'Posao majke',
     Fjob: 'Posao oca',
     activities: 'Izvannastavne aktivnosti',
     higher: 'Zeli nastaviti školovanje',
+    G3: 'Uspjeh',
 }
 
 const allFields = {
